78215fb65a33377b265980356642fee1ed34c60c
safinasp <safinapratiwi.sp@gmail.com>
2021-09-28 11:55:16 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		System.out.println("Nama  : Safina");
+		System.out.println("NIM   : 6706213030");
+		System.out.println("Kelas : D3IF-45-01");
+	}
+}
+	
